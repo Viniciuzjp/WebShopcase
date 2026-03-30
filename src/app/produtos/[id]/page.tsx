a9b211c45570b9bf13ace9c472b38b348094ae3a
Vinicius Juarez <viniciusjuarez720@gmail.com>
2026-03-29 21:04:01 -0300

    feat(skeleton) add skeleton component while the page remains suspended

--- src/app/produtos/[id]/page.tsx
+++ src/app/produtos/[id]/page.tsx
@@ -8,6 +8,8 @@ import Button from "@/components/button/Button";
 import { Products } from "@/ui/shopifyinterface/interface";
 import Suggestions from "@/components/suggestions/suggestions";
 import { Text } from "@/components/text/Text";
+import { Skeleton } from "@/design-system/layout/Skeleton";
+import { Container } from "@/design-system/layout/Container";
 
 export default function ProductPage() {
   const { id } = useParams();
@@ -21,7 +23,6 @@ export default function ProductPage() {
   useEffect(() => {
     if (!id) {
       setError("ID do produto não fornecido");
-      setLoading(false);
       return;
     }
 
@@ -61,14 +62,6 @@ export default function ProductPage() {
     AddItem(item);
   };
 
-  if (loading) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <Text variant="h1">Carregando...</Text>
-      </div>
-    );
-  }
-
   if (error) {
     return (
       <div className="flex items-center justify-center min-h-screen">
@@ -77,67 +70,105 @@ export default function ProductPage() {
     );
   }
 
-  if (!product) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <Text variant="h1">Textroduto não encontrado</Text>
-      </div>
-    );
-  }
-  const currentImage = product.images[currentImageIndex];
+  const currentImage = product?.images[currentImageIndex];
 
   return (
-    <main className="flex flex-col xl:px-10 max-lg:px-5 gap-10">
-      <section className="flex xl:px-10 max-lg:flex-col w-full">
-        <section className="flex lg:sticky xl:px-10 lg:top-15 lg:h-fit py-10 gap-2 lg:px-3 lg:w-7/10 ">
-          <div className="flex gap-2 py-2 max-lg:w-2/10 max-xl:w-2/10 xl:w-1/6 flex-col">
-            {product.images.map((p, index) => (
-              <Image
-                key={p.src}
-                src={p.src}
-                alt={p.altText || "IMG"}
-                height={p.height}
-                width={p.width}
-                className="hover:border"
-                onClick={() => setCurrentImageIndex(index)}
-              ></Image>
-            ))}
-          </div>
-          <div className="w-full">
-            <Image
-              src={currentImage.src}
-              alt={product.images[0].altText || "IMG"}
-              height={product.images[0].height}
-              width={product.images[0].width}
-            ></Image>
-          </div>
-        </section>
-        <section className="flex lg:p-8 gap-10 flex-col lg:w-7/10 w-full py-5">
-          <Text variant="h1">{product.title}</Text>
-          <hr className="text-neutral-300" />
-          <Text variant="h1" >
-            R$ {product.variants[0].price.amount}
-          </Text>
+    <>
+      {loading ? (
+        <>
+          <Container>
+            <section className="flex xl:px-10 max-lg:flex-col w-full">
+              <section className="flex lg:sticky xl:px-10 lg:top-15 lg:h-fit py-10 gap-2 lg:px-3 lg:w-7/10 ">
+                <div className="flex gap-2 py-2 max-lg:w-2/10 max-xl:w-2/10 xl:w-1/6 flex-col">
+                  {Array.from({ length: 5 }).map((_, i) => (
+                    <div key={i}>
+                      <Skeleton className="w-full h-30" />
+                    </div>
+                  ))}
+                </div>
+                <div className="w-full">
+                  {Array.from({ length: 1 }).map((_, i) => (
+                    <div key={i}>
+                      <Skeleton className="w-full h-160" />
+                    </div>
+                  ))}
+                </div>
+              </section>
+              <section className="flex lg:p-8 gap-10 flex-col lg:w-7/10 w-full py-5">
+                <div className="space-y-2">
+                  <Skeleton className="w-9/10 h-10" />
+                  <Skeleton className="w-7/10 h-10" />
+                  <Skeleton className="w-4/10 h-10" />
+                </div>
+                <Skeleton className="w-3/10 h-10" />
 
-          <Button onClick={handleAddToCart}>Adicionar Ao Carrinho</Button>
+                <Skeleton className="w-full h-15" />
 
-          <div className="flex flex-col gap-2">
-            <Text variant="h2">
-              ESPECIFICAÇÕES
-            </Text>
-            <div
-              className="text-sm font-medium text-neutral-700 space-y-4"
-              dangerouslySetInnerHTML={{
-                __html: product.descriptionHtml || "",
-              }}
-            />
+                <div className="flex flex-col gap-2">
+                  <Skeleton className="full h-10" />
+                  <div
+                    className="text-sm font-medium text-neutral-700 space-y-4"
+                    dangerouslySetInnerHTML={{
+                      __html: product?.descriptionHtml || "",
+                    }}
+                  />
+                </div>
+              </section>
+            </section>
+          </Container>
+        </>
+      ) : (
+        <Container>
+          <section className="flex xl:px-10 max-lg:flex-col w-full">
+            <section className="flex lg:sticky xl:px-10 lg:top-15 lg:h-fit py-10 gap-2 lg:px-3 lg:w-7/10 ">
+              <div className="flex gap-2 py-2 max-lg:w-2/10 max-xl:w-2/10 xl:w-1/6 flex-col">
+                {product?.images.map((p, index) => (
+                  <Image
+                    key={p.src}
+                    src={p.src}
+                    alt={p.altText || "IMG"}
+                    height={p.height}
+                    width={p.width}
+                    className="hover:border"
+                    onClick={() => setCurrentImageIndex(index)}
+                  ></Image>
+                ))}
+              </div>
+              <div className="w-full">
+                {currentImage && (
+                  <Image
+                    src={currentImage.src}
+                    alt={product.images[0].altText || "IMG"}
+                    height={product.images[0].height}
+                    width={product.images[0].width}
+                  ></Image>
+                )}
+              </div>
+            </section>
+            <section className="flex lg:p-8 gap-10 flex-col lg:w-7/10 w-full py-5">
+              <Text variant="h1">{product?.title}</Text>
+              <hr className="text-neutral-300" />
+              <Text variant="h1">R$ {product?.variants[0].price.amount}</Text>
+
+              <Button onClick={handleAddToCart}>Adicionar Ao Carrinho</Button>
+
+              <div className="flex flex-col gap-2">
+                <Text variant="h2">ESPECIFICAÇÕES</Text>
+                <div
+                  className="text-sm font-medium text-neutral-700 space-y-4"
+                  dangerouslySetInnerHTML={{
+                    __html: product?.descriptionHtml || "",
+                  }}
+                />
+              </div>
+            </section>
+          </section>
+          <Text variant="h2">Você pode gostar</Text>
+          <div className="gap-5 justify-center items-center">
+            <Suggestions />
           </div>
-        </section>
-      </section>
-        <Text variant="h2">Você pode gostar</Text>
-        <div className="gap-5 justify-center items-center">
-          <Suggestions />
-        </div>
-    </main>
+        </Container>
+      )}
+    </>
   );
 }
